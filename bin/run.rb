0258bb897c6c830141bcd12b7f052dd9ee369545
Matthew Ream <matthewtimothyream@gmail.com>
2019-09-10 17:08:45 -0500

    Done.

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,14 @@
+require_relative '../config/environment'
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
+
+source "https://rubygems.org"
+gem "rspec"
+gem "sinatra"
+ 
+group :development do
+gem "pry"
+end
